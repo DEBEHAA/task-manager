a488b9b299617b17fbe492ff60d798cc1850053f
DEBEHAA <debehaaj.22cse@kongu.edu>
2025-05-24 14:04:26 +0530

    sec

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ const app = express()
 // Middleware
 app.use(express.json())
 app.use(cors())
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}))
 
 app.use(morgan("dev"))
 
